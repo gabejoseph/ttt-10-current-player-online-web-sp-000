a2c2132835c95c7829b899478cd4e33c23ad4958
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-03-04 01:07:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count % 2 == 0 
+  if turn_count(board) % 2 == 0 
     puts "X"
-  else turn_count % 2 == 1 
+  else turn_count(board) % 2 == 1 
     puts "O"
   end
 end
